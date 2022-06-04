9f63ebe9815cc958798cc0ae30c7309ecafe3e54
Michael Reis <m.christian279@gmail.com>
2022-06-04 11:12:02 -0300

    refact: add playbook ansible to config ec2 instance

--- terraform/ec2_instance_inventory.tf
+++ terraform/ec2_instance_inventory.tf
@@ -1,7 +1,13 @@
 resource "local_file" "ansible_inventory" {
   content = templatefile("./ansible/inventory.tmpl", {
     ip          = aws_instance.appliance.public_ip,
-    ssh_keyfile = "./${var.generated_key_name}.pem"
+    ssh_keyfile = "ansible/${var.generated_key_name}.pem"
   })
   filename = format("%s/%s", abspath(path.root), "./ansible/inventory.yaml")
+
+  depends_on = [local_file.ansible_inventory]
+  provisioner "local-exec" {
+      command = "sleep 5; ansible-playbook -i ./ansible/inventory.yaml  ./ansible/playbook.yaml"
+    }
 }
+
